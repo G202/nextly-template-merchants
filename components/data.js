a916f92e3a0467409bc52c942ccf8da2d87ac188
G202 <grantcollins@protonmail.com>
2023-08-02 08:55:38 +1000

    Test content update

--- components/data.js
+++ components/data.js
@@ -11,8 +11,8 @@ import benefitOneImg from "../public/img/benefit-one.png";
 import benefitTwoImg from "../public/img/benefit-two.png";
 
 const benefitOne = {
-  title: "Highlight your benefits",
-  desc: "You can use this space to highlight your first benefit or a feature of your product. It can also contain an image or Illustration like in the example along with some bullet points.",
+  title: "Delivering long-term value",
+  desc: "Whilst we believe art is a vital part of an NFT project, we advocate that utility is the key factor that will ensure your token continues to increase in value.",
   image: benefitOneImg,
   bullets: [
     {
